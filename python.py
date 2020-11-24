007c9087666c2e420c23169084b812101b3aea47
eibhig <72438942+eibhig@users.noreply.github.com>
2020-11-24 02:09:34 -0800

    Update python.py

--- python.py
+++ python.py
@@ -1,5 +1,7 @@
-import datetime
+pipeline{
+  import datetime
 print("Hello world!")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
+}
