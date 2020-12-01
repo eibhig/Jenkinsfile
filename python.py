81d49c10bdd8ca17f776f55f6bc62db02dce5b00
eibhig <72438942+eibhig@users.noreply.github.com>
2020-12-01 02:29:17 -0800

    Update python.py

--- python.py
+++ python.py
@@ -1,7 +1,5 @@
 import datetime
 print("Hello world!")
-print("Take your time ")
-print("date and time")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
